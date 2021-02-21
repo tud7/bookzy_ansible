66469d3ed9808ff8722dc997adbc67a74cafbc68
student <tduongcs@gmail.com>
2021-02-21 06:28:47 +0000

    created base endpoint

--- bookzy-app/src/main/java/com/bookzy/app/bookzyapp/BookzyAppApplication.java
+++ bookzy-app/src/main/java/com/bookzy/app/bookzyapp/BookzyAppApplication.java
@@ -3,11 +3,23 @@ package com.bookzy.app.bookzyapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 @SpringBootApplication
 public class BookzyAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookzyAppApplication.class, args);
 	}
+}
 
+@RestController
+class GreetingController {
+    
+    @RequestMapping("/")
+	public String index() {
+		return "Hello from Bookzy by Tu Duong!";
+	}
 }
